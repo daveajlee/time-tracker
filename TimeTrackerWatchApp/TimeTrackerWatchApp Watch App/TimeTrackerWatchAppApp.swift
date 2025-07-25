d4ef157da3e0f74b417f41348ed53043094dd672
David A J Lee <dave@davelee.de>
2025-07-25 11:05:00 +0200

    Initial WatchOS App

---
+++ TimeTrackerWatchApp/TimeTrackerWatchApp Watch App/TimeTrackerWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerWatchAppApp.swift
+//  TimeTrackerWatchApp Watch App
+//
+//  Created by Dave Lee on 25.07.25.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackerWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TimeTrackerWatchApp/TimeTrackerWatchApp Watch AppTests/TimeTrackerWatchApp_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerWatchApp_Watch_AppTests.swift
+//  TimeTrackerWatchApp Watch AppTests
+//
+//  Created by Dave Lee on 25.07.25.
+//
+
+import Testing
+@testable import TimeTrackerWatchApp_Watch_App
+
+struct TimeTrackerWatchApp_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
